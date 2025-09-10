d7f0e369c69cfaa6170edfd5ca6babffa5cf1f83
scientist-Momi <scientistmomi22@gmail.com>
2025-09-10 05:29:32 +0100

    crafting structure

--- src/main/java/com/koob/Koob_backend/book/BookController.java
+++ src/main/java/com/koob/Koob_backend/book/BookController.java
@@ -1,4 +1,30 @@
 package com.koob.Koob_backend.book;
 
+import com.koob.Koob_backend.user.User;
+import com.koob.Koob_backend.userLibrary.UserLibraryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/v1/books")
 public class BookController {
+    private final UserLibraryService userLibraryService;
+
+    public BookController(UserLibraryService userLibraryService) {
+        this.userLibraryService = userLibraryService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> saveBookForUser(@RequestBody Book book, @AuthenticationPrincipal User user) {
+        userLibraryService.addBookToUser(user.getId(), book);
+        return ResponseEntity.ok("Book saved!");
+    }
+
+    @GetMapping("/mine")
+    public ResponseEntity<Set<Book>> getMyBooks(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user.getBooks());
+    }
 }
