b483061c2f36aaaae186d2005aea7bca0b6358ef
scientist-Momi <scientistmomi22@gmail.com>
2025-09-15 15:03:57 +0100

    working on book service

--- src/main/java/com/koob/Koob_backend/book/Book.java
+++ src/main/java/com/koob/Koob_backend/book/Book.java
@@ -2,8 +2,10 @@ package com.koob.Koob_backend.book;
 
 import com.koob.Koob_backend.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -16,6 +18,8 @@ import java.util.Set;
 @Data
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "books")
 public class Book {
     @Id
