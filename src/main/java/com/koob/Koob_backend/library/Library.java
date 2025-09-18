f20f83c6c59de6daf7260ecec713282193e9ba25
scientist-Momi <scientistmomi22@gmail.com>
2025-09-18 12:20:57 +0100

    working on library service

--- src/main/java/com/koob/Koob_backend/library/Library.java
+++ src/main/java/com/koob/Koob_backend/library/Library.java
@@ -1,4 +1,11 @@
 package com.koob.Koob_backend.library;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "libraries")
 public class Library {
 }
