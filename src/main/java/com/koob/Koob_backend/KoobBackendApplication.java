7685a024e24f44298871a77571a0f4973860ab08
scientist-Momi <scientistmomi22@gmail.com>
2025-10-01 16:47:02 +0100

    update

--- src/main/java/com/koob/Koob_backend/KoobBackendApplication.java
+++ src/main/java/com/koob/Koob_backend/KoobBackendApplication.java
@@ -27,24 +27,4 @@ public class KoobBackendApplication {
 		SpringApplication.run(KoobBackendApplication.class, args);
 	}
 
-//    @Bean
-//    public CommandLineRunner runner(ChatClient.Builder builder) {
-//        return args -> {
-//            ChatClient chatClient = builder.build();
-//
-//            ToolCallback[] bookTools = ToolCallbacks.from(new BookTools());
-//            ChatResponse response = chatModel.call(
-//                    new Prompt(
-//                            "Use the tool to search for books about world war 2 and save the result to a file. Also show me the query you provided to the tools",
-//                            OpenAiChatOptions.builder()
-//                                    .toolCallbacks(bookTools)
-//                                    .model("gpt-4o-mini")
-//                                    .temperature(0.4)
-//                                    .build()
-//                    ));
-//
-//            System.out.println(response.getResults());
-//        };
-//    }
-
 }
